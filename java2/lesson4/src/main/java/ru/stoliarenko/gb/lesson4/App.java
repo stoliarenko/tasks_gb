566e2b0d5895af56a2c64d9fa520d09005d95068
Alexander <lostaztecian@mail.ru>
2018-09-24 21:37:44 +0300

    extended base

--- java2/lesson4/src/main/java/ru/stoliarenko/gb/lesson4/App.java
+++ java2/lesson4/src/main/java/ru/stoliarenko/gb/lesson4/App.java
@@ -1,7 +1,11 @@
 package ru.stoliarenko.gb.lesson4;
 
 import javax.swing.*;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class App extends JFrame{
@@ -9,10 +13,46 @@ public class App extends JFrame{
         App app = new App();
         app.setVisible(true);
     }
-    private App() {
+    private App() throws HeadlessException{
         setTitle("Pony Chat");
         setSize(640, 480);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        useNimbus();
+        
+        final JButton exitButton = new JButton("Quit");
+        exitButton.setSize(60, 40);
+        exitButton.setToolTipText("EXIT");
+        exitButton.addActionListener(event -> System.exit(0));
+        
+        final JPanel leftPanel = new JPanel();
+        final JPanel rightPanel = new JPanel();
+        final JPanel topPanel = new JPanel();
+        final JPanel bottomPanel = new JPanel();
+        final JPanel mainPanel = new JPanel();
+        getContentPane().add(mainPanel, BorderLayout.CENTER);
+        getContentPane().add(topPanel, BorderLayout.NORTH);
+        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+        getContentPane().add(leftPanel, BorderLayout.WEST);
+        getContentPane().add(rightPanel, BorderLayout.EAST);
+        mainPanel.setBackground(Color.CYAN);
+        leftPanel.setBackground(Color.green);
+        rightPanel.setBackground(Color.RED);
+        topPanel.setBackground(Color.blue);
+        bottomPanel.setBackground(Color.MAGENTA);
+        
+        bottomPanel.add(exitButton, BorderLayout.LINE_END);
+    }
+    private void useNimbus() {
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
     }
 }
