7b237a858e105aaa972fab6c5dfa265270f504bb
Stoliarenko Alexander <lostaztecian@mail.ru>
2018-09-25 20:43:25 +0300

    working project before refactoring

--- java2/lesson4/src/main/java/ru/stoliarenko/gb/lesson4/App.java
+++ java2/lesson4/src/main/java/ru/stoliarenko/gb/lesson4/App.java
@@ -4,44 +4,111 @@ import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 
 public class App extends JFrame{
-    public static void main( String[] args )    {
+    private final static Color HIGH_RED = new Color(0xF0, 0x62, 0x92);
+    private final static Color LOW_RED = new Color(0xF4, 0x8F, 0xB1);//#F48FB1
+    private final static Color HIGH_PURPLE = new Color(0x8E, 0x24, 0xAA);//#8E24AA
+    private final static Color LOW_PURPLE = new Color(0xAB, 0x47, 0xBC);//#BA68C8
+    private final int MAIN_WONDOW_SIZE = 100;
+    private final int USERS_WINDOW_SIZE = (int)(MAIN_WONDOW_SIZE * 0.2);
+    
+    public static void main( String[] args ) {
         App app = new App();
         app.setVisible(true);
     }
+    
     private App() throws HeadlessException{
-        setTitle("Pony Chat");
-        setSize(640, 480);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setupBaseFrame();
         useNimbus();
         
-        final JButton exitButton = new JButton("Quit");
-        exitButton.setSize(60, 40);
-        exitButton.setToolTipText("EXIT");
-        exitButton.addActionListener(event -> System.exit(0));
+        final JPanel basePanel = new JPanel(new GridBagLayout());
+        basePanel.setBackground(LOW_PURPLE);
+        setContentPane(basePanel);
         
-        final JPanel leftPanel = new JPanel();
-        final JPanel rightPanel = new JPanel();
-        final JPanel topPanel = new JPanel();
-        final JPanel bottomPanel = new JPanel();
-        final JPanel mainPanel = new JPanel();
-        getContentPane().add(mainPanel, BorderLayout.CENTER);
-        getContentPane().add(topPanel, BorderLayout.NORTH);
-        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
-        getContentPane().add(leftPanel, BorderLayout.WEST);
-        getContentPane().add(rightPanel, BorderLayout.EAST);
-        mainPanel.setBackground(Color.CYAN);
-        leftPanel.setBackground(Color.green);
-        rightPanel.setBackground(Color.RED);
-        topPanel.setBackground(Color.blue);
-        bottomPanel.setBackground(Color.MAGENTA);
         
-        bottomPanel.add(exitButton, BorderLayout.LINE_END);
+        final JTextArea chatHistory = new JTextArea();
+        final JScrollPane chatHistoryScrollPane = new JScrollPane(chatHistory);
+        final JTextArea users = new JTextArea();
+        final JScrollPane usersScrollPane = new JScrollPane(users);
+        final JButton enterButton = new JButton("Enter", new ImageIcon("src/resources/Pinkie_Pie_small.png"));
+        final JTextField inputField = new JTextField();
+        
+        GridBagConstraints chatHistoryConstraints = new GridBagConstraints();
+        GridBagConstraints usersConstraints = new GridBagConstraints();
+        GridBagConstraints enterButtonConstraints = new GridBagConstraints();
+        GridBagConstraints inputFieldConstraints = new GridBagConstraints();
+////////////////////////////////////////////////////////////////////////////// 
+        chatHistoryConstraints.anchor =  GridBagConstraints.FIRST_LINE_START;
+        chatHistoryConstraints.fill = GridBagConstraints.BOTH;
+        chatHistoryConstraints.weightx = 1;
+        chatHistoryConstraints.weighty = 1;
+        chatHistoryConstraints.gridwidth = MAIN_WONDOW_SIZE;
+        chatHistoryConstraints.gridheight = MAIN_WONDOW_SIZE;
+        chatHistoryConstraints.gridx = 0;
+        chatHistoryConstraints.gridy = 0;
+        
+        chatHistoryScrollPane.setBorder(null);
+        chatHistory.setBackground(LOW_RED);
+        chatHistory.setForeground(LOW_PURPLE);
+        chatHistory.setBorder(BorderFactory.createTitledBorder("Messages:"));
+        chatHistory.setEditable(false);
+        chatHistory.setAutoscrolls(true);
+///////////////////////////////////////////////////////////////////////////////        
+        enterButtonConstraints.fill = GridBagConstraints.BOTH;
+        enterButtonConstraints.gridwidth = USERS_WINDOW_SIZE;
+        enterButtonConstraints.gridx = MAIN_WONDOW_SIZE;
+        enterButtonConstraints.gridy = MAIN_WONDOW_SIZE;
+
+        enterButton.setForeground(HIGH_PURPLE);
+        enterButton.addActionListener((event)->{
+            System.out.println(inputField.getText());
+            chatHistory.append(inputField.getText() + "\n");
+            inputField.setText("");
+        });
+///////////////////////////////////////////////////////////////////////////////
+        usersConstraints.anchor = GridBagConstraints.LINE_END;
+        usersConstraints.fill = GridBagConstraints.BOTH;
+        usersConstraints.gridwidth = USERS_WINDOW_SIZE;
+        usersConstraints.gridheight = MAIN_WONDOW_SIZE;
+        
+        usersScrollPane.setBorder(null);
+        users.setBackground(LOW_RED);
+        users.setForeground(HIGH_PURPLE);
+        users.setFont(users.getFont().deriveFont(Font.BOLD, users.getFont().getSize()+1));
+        users.setBorder(BorderFactory.createTitledBorder("Users:"));
+        users.setEditable(false);
+        users.append("Alexander Radionovoch" + "\n");
+///////////////////////////////////////////////////////////////////////////////
+        inputFieldConstraints.fill = GridBagConstraints.BOTH;
+        inputFieldConstraints.gridwidth = MAIN_WONDOW_SIZE;
+        inputFieldConstraints.gridx = 0;
+        inputFieldConstraints.gridy = MAIN_WONDOW_SIZE;
+        inputFieldConstraints.ipady = 10;
+        
+        inputField.setBackground(LOW_PURPLE);
+        inputField.setForeground(LOW_RED);
+        inputField.addActionListener((event)->{
+            System.out.println(inputField.getText());
+            chatHistory.append(inputField.getText() + "\n");
+            inputField.setText("");
+        });
+        inputField.setBorder(BorderFactory.createEtchedBorder());
+///////////////////////////////////////////////////////////////////////////////
+        
+        
+        basePanel.add(chatHistoryScrollPane, chatHistoryConstraints);
+        basePanel.add(usersScrollPane, usersConstraints);
+        basePanel.add(enterButton, enterButtonConstraints);
+        basePanel.add(inputField, inputFieldConstraints);
+    }
+    private void setupBaseFrame() {
+        final ImageIcon icon = new ImageIcon("src/resources/Pinkie Pie.png");
+        setIconImage(icon.getImage());
+        setTitle("Pony Chat");
+        setSize(1024, 768);setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     private void useNimbus() {
         try {
