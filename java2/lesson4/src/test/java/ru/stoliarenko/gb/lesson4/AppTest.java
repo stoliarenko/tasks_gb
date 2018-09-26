57accd8ecf8d7a51e29fc97cfe8f888f6d60772f
Alexander Stolyarenko <36860575+LostAztecian@users.noreply.github.com>
2018-09-26 13:29:09 +0300

    Update AppTest.java

--- java2/lesson4/src/test/java/ru/stoliarenko/gb/lesson4/AppTest.java
+++ java2/lesson4/src/test/java/ru/stoliarenko/gb/lesson4/AppTest.java
@@ -5,7 +5,8 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Понятия не имею как писать тесты на графический интерфейс
+ * оставляю класс в качестве затычки для maven
  */
 public class AppTest 
     extends TestCase
