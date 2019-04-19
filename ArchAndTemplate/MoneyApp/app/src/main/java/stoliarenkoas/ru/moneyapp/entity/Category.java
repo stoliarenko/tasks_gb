aeb77f194c05f5cee3d1b390e112a79cdfc0a689
Alexander <lostaztecian@mail.ru>
2019-04-19 13:14:50 +0300

    Order builder added.

---
+++ ArchAndTemplate/MoneyApp/app/src/main/java/stoliarenkoas/ru/moneyapp/entity/Category.java
@@ -0,0 +1,14 @@
+package stoliarenkoas.ru.moneyapp.entity;
+
+public enum Category {
+    UPKEEP,
+    CLOTH,
+    CONSUMABLE,
+    FOOD_BASIC,
+    FOOD_LUX,
+    FOOD_JUNK,
+    BAD_HABIT,
+    SUBSCRIPTION,
+    GIFT,
+    MISCELLANEOUS
+}
---
+++ ArchAndTemplate/MoneyApp/app/src/main/java/stoliarenkoas/ru/moneyapp/entity/Order.java
@@ -0,0 +1,17 @@
+package stoliarenkoas.ru.moneyapp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Order {
+
+    private long time;
+    private Category type;
+    private String description;
+    private double price;
+
+    Order(long time){ this.time = time; }
+
+}
